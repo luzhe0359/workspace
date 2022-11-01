89f86f551fa4938336e74610847d56582038096b
luzhe0359 <luzhe0359@163.com>
2022-11-01 17:27:40 +0800

    🆕自定义BannerWebpackPlugin

---
+++ webpack-parse/plugin/plugins/banner-webpack-plugin.js
@@ -0,0 +1,47 @@
+class BannerWebpackPlugin {
+  constructor(options = {}) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    compiler.hooks.emit.tap("BannerWebpackPlugin", (compliation) => {
+      debugger;
+      const extensions = ["css", "js"];
+      // 1. 获取即将输出的资源文件，compliation.assets
+      // 2. 过滤只保留js和css资源
+      const assets = Object.keys(compliation.assets).filter((assetPath) => {
+        // 将文件名分割 ['xxx','js'] ['xxx','css']
+        const splitted = assetPath.split(".");
+        // 获取最后一个文件扩展名
+        const extension = splitted[splitted.length - 1];
+        // 判断是否存在
+        return extensions.includes(extension);
+      });
+      console.log(assets);
+      // 3. 遍历并添加注释
+      const prefix = `/*
+* Author: ${this.options.author}
+*/
+`;
+      assets.forEach((asset) => {
+        // 获取原来的内容
+        const source = compliation.assets[asset].source();
+        // 拼接上注释
+        const content = prefix + source;
+
+        // 修改资源
+        compliation.assets[asset] = {
+          // 最终资源输出时，调用source方法（source方法的返回值就是资源的内容）
+          source() {
+            return content;
+          },
+          size() {
+            return content.length;
+          },
+        };
+      });
+    });
+  }
+}
+
+module.exports = BannerWebpackPlugin;
--- webpack-parse/plugin/webpack.config.js
+++ webpack-parse/plugin/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const TestPlugin = require("./plugins/test-plugin");
+const BannerWebpackPlugin = require("./plugins/banner-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "./src/main.js"),
@@ -9,12 +10,22 @@ module.exports = {
     filename: "js/[name].js",
     clean: true,
   },
-  module: {},
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "./public/index.html"),
     }),
-    new TestPlugin(), // 成功输出打印信息
+    // new TestPlugin(), // 成功输出打印信息
+    new BannerWebpackPlugin({
+      author: "小路",
+    }),
   ],
-  mode: "development",
+  mode: "production",
 };
