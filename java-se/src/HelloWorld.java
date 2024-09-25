6aae848428806c621d5a9071a1cc54c6a0025990
luzhe0359 <luzhe0359@163.com>
2024-09-25 17:40:27 +0800

    🔨java概述、变量与运算符、流程控制语句

--- java-se/src/HelloWorld.java
+++ java-se/src/HelloWorld.java
@@ -1,5 +1,9 @@
+import java.util.Scanner;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world !!!");
+
+        Scanner scann = new Scanner(System.in);
     }
 }
