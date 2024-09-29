406441212672847c2a30f2bed53073f0bcfa5122
luzhe0359 <luzhe0359@163.com>
2024-09-29 16:56:33 +0800

    🔧熟悉idea各项配置

---
+++ java-se/chapter04/src/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ * ClassName: HelloWorld
+ * Package: PACKAGE_NAME
+ * Description:
+ *
+ * @Author luzhe0359
+ * @Create 2024/9/29 16:48
+ * @Version 1.0
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hi! luzhe0359 ");
+        System.out.println("熟悉 idea 各项配置 ~");
+    }
+}
