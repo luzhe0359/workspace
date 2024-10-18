4db4d1f126bb6895a0745a33e98c44adb8efa1af
luzhe <luzhe@linewell.com>
2024-10-18 17:41:00 +0800

    🎨方法扩展，重载、可变个数形参、值传递机制、递归

---
+++ java-se/chapter06_oop1/src/_05_method_more/_01overload/OverloadTest.java
@@ -0,0 +1,57 @@
+package _05_method_more._01overload;
+
+/**
+ * ClassName: OverloadTest
+ * Package: _05_method_more._01overload
+ * Description:
+ *
+ * @Author luzhe0359
+ * @Create 2024/10/18 16:48
+ * @Version 1.0
+ */
+public class OverloadTest {
+    public static void main(String[] args) {
+
+        OverloadTest test = new OverloadTest();
+
+        test.add(1,2,3);
+
+        test.add(10,20);
+
+        test.add(10,20.0);
+
+    }
+
+
+    public void add(int i,int j){
+        System.out.println("1111");
+    }
+
+    public void add(double d1,double d2){
+        System.out.println("3333");
+    }
+
+    public void add(int i,int j,int k){
+
+    }
+
+    public void add(String s1,String s2){
+
+    }
+
+    public void add(int i,String s){
+
+    }
+
+    public void add(String s,int i){
+
+    }
+
+//    public void add(int m ,int n){
+//        System.out.println("2222");
+//    }
+
+//    public int add(int m,int n){
+//        return m + n;
+//    }
+}
