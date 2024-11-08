25718224f5720058f9e9eb0aafb8c80936f90d1a
luzhe0359 <luzhe0359@163.com>
2024-11-08 17:00:05 +0800

    🚀main方法

---
+++ java-se/chapter08_oop3/src/_03_main/MainDemo.java
@@ -0,0 +1,17 @@
+package _03_main;
+
+/**
+ * ClassName: MainDemo
+ * Description:
+ *
+ * @Author 尚硅谷-宋红康
+ * @Create 9:39
+ * @Version 1.0
+ */
+public class MainDemo {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; i++) {
+            System.out.println("hello:" + args[i]);
+        }
+    }
+}
