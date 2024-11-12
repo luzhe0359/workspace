2f2b4555e8698822ece56e65e2149f56e7d8dbc5
luzhe0359 <luzhe0359@163.com>
2024-11-12 15:01:51 +0800

    🚀final关键字

---
+++ java-se/chapter08_oop3/src/_06_final/FinalTest.java
@@ -0,0 +1,84 @@
+package _06_final;
+
+/**
+ * ClassName: FinalTest
+ * Package: _06_final
+ * Description:
+ *
+ * @Author luzhe0359
+ * @Create 2024/11/12 14:59
+ * @Version 1.0
+ */
+public class FinalTest {
+    public static void main(String[] args) {
+        E e = new E();
+        System.out.println(e.MIN_SCORE);
+//        e.MIN_SCORE = 1;
+
+        E e1 = new E(10);
+//        e1.LEFT = 11;
+    }
+}
+
+class E{
+    //成员变量
+    final int MIN_SCORE = 0;
+    final int MAX_SCORE;
+
+    final int LEFT;
+
+//    final int RIGHT;
+
+    {
+//        MIN_SCORE = 1;
+        MAX_SCORE = 100;
+    }
+
+    public E(){
+        LEFT = 2;
+    }
+    public E(int left){
+        LEFT = left;
+    }
+
+//    public void setRight(int right){
+//        RIGHT = right;
+//    }
+
+}
+
+class F{
+    public void method(){
+        final int num;
+        num = 10;
+//        num++;
+        System.out.println(num);
+    }
+
+    public void method(final int num){
+//        num++;
+        System.out.println(num);
+    }
+}
+
+
+final class A{
+
+}
+
+//class B extends A{}
+
+//class SubString extends String{}
+
+class C{
+    public final void method(){
+
+    }
+}
+
+class D extends C{
+//    public void method(){
+//
+//    }
+}
+
